03cd2810c539f17a000e4e4872ad6a0109fc6bbc
Andrew Kim <akim618@gmail.com>
2018-12-17 18:29:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,10 @@ def fizzbuzz(int)
   elsif int % 5 == 0
     return "Buzz"
 
-   int % 3 && 5 == 0
+   elsif int % 3 && 5 == 0
+    return "FizzBuzz"
+
+  elsif int % 3 || 5 == 0
     return "FizzBuzz"
 end
 end
